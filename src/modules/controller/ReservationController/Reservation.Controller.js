205b01d5d7e442b540e5089392cd42ce5d1f91bf
Abdelrahman-Mamdouh16 <bedo.lela32@gmail.com>
2024-07-17 19:02:06 +0300

    push

--- src/modules/controller/ReservationController/Reservation.Controller.js
+++ src/modules/controller/ReservationController/Reservation.Controller.js
@@ -309,15 +309,16 @@ export const createCheckoutSessionOnTelehealth = async (req, res, next) => {
                 },
             ],
             mode: 'payment',
-            success_url: `https://online-doc-app.vercel.app/reservationOnTelehealth/createCall`,
+            success_url: `https://online-doc-app.vercel.app/reservation/Thank-You`,
             cancel_url: `https://online-doc-app.vercel.app/`,
             customer_email: user.email,
             client_reference_id: user._id
+
         });
         if (!session)
-            res.json({ success: false, message: 'The Checkout Session not created' });
+            return res.json({ success: false, message: 'The Checkout Session not created' });
         else {
-            res.json({ success: true, message: 'The Checkout Session is created', result: session });
+            return res.json({ success: true, message: 'The Checkout Session is created', result: session });
         }
     } catch (error) {
         console.error(error);
@@ -383,95 +384,95 @@ export const createReservationOnTelehealth = async (req, res) => {
 
         await reservation.save();
 
-        //         const transporter = nodemailer.createTransport({
-        //             service: 'gmail',
-        //             auth: {
-        //                 user: "bedo.lela32@gmail.com",
-        //                 pass: "ekasgpofbqyvnmkj",
-        //             },
-        //         });
-        //         const info = await transporter.sendMail({
-        //             from: '"Abdelrahman Mamdouh" <bedo.lela32@gmail.com>', // sender address
-        //             to: `${user.email}`, // list of receivers
-        //             subject: "Booking Confirmation on Online Doctor app", // Subject line
-        //             // text: "Hello world?", // plain text body
-        //             html: `<!DOCTYPE html>
-        // <html lang="en">
-        // <head>
-        //   <meta charset="UTF-8">
-        //   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        //   <title>Email with Logo, Image, and Text</title>
-        //   <style>
-        //     /* General email styles */
-        //     body {
-        //       font-family: Arial, sans-serif;
-        //       margin: 0;
-        //       padding: 0;
-        //       background-color: #f4f4f4;
-        //     }
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: "bedo.lela32@gmail.com",
+                pass: "ekasgpofbqyvnmkj",
+            },
+        });
+        const info = await transporter.sendMail({
+            from: '"Abdelrahman Mamdouh" <bedo.lela32@gmail.com>', // sender address
+            to: `${user.email}`, // list of receivers
+            subject: "Booking Confirmation on Online Doctor app", // Subject line
+            // text: "Hello world?", // plain text body
+            html: `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Email with Logo, Image, and Text</title>
+          <style>
+            /* General email styles */
+            body {
+              font-family: Arial, sans-serif;
+              margin: 0;
+              padding: 0;
+              background-color: #f4f4f4;
+            }
 
-        //     /* Container styles */
-        //     .container {
-        //       width: 600px; /* Adjust width as needed */
-        //       margin: 0 auto; /* Center the container */
-        //       background-color: #fff;
-        //       padding: 20px;
-        //     }
+            /* Container styles */
+            .container {
+              width: 600px; /* Adjust width as needed */
+              margin: 0 auto; /* Center the container */
+              background-color: #fff;
+              padding: 20px;
+            }
 
-        //     .logo img {
-        //       width: 200px; /* Adjust logo width */
-        //       height: auto; /* Maintain aspect ratio */
-        //     }
+            .logo img {
+              width: 200px; /* Adjust logo width */
+              height: auto; /* Maintain aspect ratio */
+            }
 
-        //     /* Image styles */
-        //     .image {
-        //       display: block;
-        //       margin-bottom: 20px;
-        //     }
+            /* Image styles */
+            .image {
+              display: block;
+              margin-bottom: 20px;
+            }
 
-        //     .image img {
-        //       width: 100%; /* Make image responsive */
-        //       height: auto; /* Maintain aspect ratio */
-        //     }
+            .image img {
+              width: 100%; /* Make image responsive */
+              height: auto; /* Maintain aspect ratio */
+            }
 
-        //     /* Text styles */
-        //     .text {
-        //       font-size: 16px;
-        //       line-height: 1.5;
-        //       color: #333;
-        //     }
-        //   </style>
-        // </head>
-        // <body>
-        //   <div class="container">
+            /* Text styles */
+            .text {
+              font-size: 16px;
+              line-height: 1.5;
+              color: #333;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
 
-        //     <div class="image">
-        //       <img src="https://res.cloudinary.com/dvr3bltmg/image/upload/v1720020782/TopOffers/a0kf3pdiolysettql2kr.png" alt="Image">
-        //     </div>
+            <div class="image">
+              <img src="https://res.cloudinary.com/dvr3bltmg/image/upload/v1720020782/TopOffers/a0kf3pdiolysettql2kr.png" alt="Image">
+            </div>
 
-        //     <div class="text">
-        //         <h2> Dear ${user.name},</h2>
-        //                     <p>Your booking has been confirmed!</p> 
-        //                     <p>Details:</p>
-        //                     <p>- Doctor: ${doctor.name}</p>
-        //                     <p>- Date: ${reservation.date}</p>
-        //                     <p>- Time: ${reservation.timeStart} - ${reservation.timeEnd}</p>
-        //                     <h3>On Telehealth Call</h3>
-        //                     <p>To show this Reservation or Delete it you can check this link : 
-        //                     <br/>
-        //                     https://online-doc-app.vercel.app/Account/getAppointment/${reservation._id}</p>
-        //                     <p>Thank you for choosing our services!</p>
-        //                     Sincerely,
-        //                     The Booking Team of Online Doctor app 
-        //                     <h3>BY : Abdelrahman Mamdouh</h3>
-        //                     </div>
-        //   </div>
-        // </body>
-        // </html>
-        // `
+            <div class="text">
+                <h2> Dear ${user.name},</h2>
+                            <p>Your booking has been confirmed!</p> 
+                            <p>Details:</p>
+                            <p>- Doctor: ${doctor.name}</p>
+                            <p>- Date: ${reservation.date}</p>
+                            <p>- Time: ${reservation.timeStart} - ${reservation.timeEnd}</p>
+                            <h3>On Telehealth Call</h3>
+                            <p>To show this Reservation or Delete it you can check this link : 
+                            <br/>
+                            https://online-doc-app.vercel.app/Account/getAppointment/${reservation._id}</p>
+                            <p>Thank you for choosing our services!</p>
+                            Sincerely,
+                            The Booking Team of Online Doctor app 
+                            <h3>BY : Abdelrahman Mamdouh</h3>
+                            </div>
+          </div>
+        </body>
+        </html>
+        `
 
-        //             , // html body
-        //         });
+            , // html body
+        });
 
         return res.json({ success: true, message: 'Reservation created successfully' });
     } catch (error) {
